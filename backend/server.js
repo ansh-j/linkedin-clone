5c25539013b9bb0b101664a064006de6a27bb89b
Ansh Jasrotia <anshvardhanavj@gmail.com>
2024-10-30 02:25:05 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,23 @@ import connectionRoutes from "./routes/connection.route.js"
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import cors from "cors"
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL,
-        credentials: true,
-    })
-);
+if (process.env.NODE_ENV !== 'production') {
+    app.use(
+        cors({
+            origin: process.env.CLIENT_URL,
+            credentials: true,
+        })
+    );
+}
+
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 
@@ -29,6 +34,14 @@ app.use(`/api/v1/users`, userRoutes)
 app.use(`/api/v1/posts`, postRoutes)
 app.use(`/api/v1/notifications`, notificationRoutes)
 app.use(`/api/v1/connections`, connectionRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
