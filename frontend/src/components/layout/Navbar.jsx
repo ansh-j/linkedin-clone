060674c060962103972d15ea8a917e9a6043e66e
Ansh Jasrotia <anshvardhanavj@gmail.com>
2024-11-17 02:58:06 +0530

    navbar changes

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -65,7 +65,7 @@ const Navbar = () => {
               />
             </Link>
           </div>
-          <div className="flex items-center gap-2 md:gap-6">
+          <div className="flex items-center gap-5 md:gap-6">
             {authUser ? (
               <>
                 <Link
